9f8b4d7806b3560bafece4146962924531c282b6
Leesa <18817396285@163.com>
2019-09-11 21:19:09 +0800

    login 校验规则way2---提示信息不会重复，适用复杂校验【优选】

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -8,7 +8,29 @@ import './index.less'
 @Form.create()
 class Login extends Component {
 
-
+    /**
+     * 自定义表单校验规则-----【错误提示不会重复，适用于复杂校验】
+     * @param rule 规则，包含了表单项的字段：field,Fullfield,type
+     * @param value  表单项的值
+     * @param callback  回调函数必须调用！！！ 当callback传参时，说明校验失败，并提示传入的参数信息；没有传参，说明校验成功。
+     */
+    validator = (rule,value,callback)=>{
+        const name = rule.field === 'username'?'用户名':'密码';
+        if(!value){
+            return callback(`${name}不能为空`)
+        }
+        if(value.length<3){
+            return callback(`${name}长度不能少于三位`)
+        }
+        if(value.length>18){
+            return callback(`${name}长度不能超过18位`)
+        }
+        const reg=/^[a-zA-Z0-9]{3,18}$/ ;
+        if(!reg.test(value)){
+            return callback(`${name}只能包含英文，数字，下划线`)
+        }
+        callback() // callback必须调用
+    }
 
     render() {
         // 因为使用了Form.create方法，Login组件上面有了form属性，此处通过解构赋值获取form属性里面的 getFieldDecorator方法
@@ -29,10 +51,14 @@ class Login extends Component {
                                     'username',
                                     {
                                         rules:[
+                                            /*  只适用于简单校验
                                             {required:true,message:'请输入用户名'},
                                             {min:3,message:'用户名不能少于三位'},
                                             {max:18,message:'用户名不能超过18位'},
                                             {pattern:/^[a-zA-Z0-9]{3,18}$/,message:'用户名只能包含英文，数字，下划线'}
+                                            */
+
+                                            {validator:this.validator}
                                         ]
                                     }
                                 )( <Input perfix={<Icon type="user"/>} placeholder="用户名"/> )
@@ -45,10 +71,12 @@ class Login extends Component {
                                     'password',
                                     {
                                         rules: [
-                                            {required:true,message:'请输入密码'},
+                                            /*{required:true,message:'请输入密码'},
                                             {min:3,message:'密码不能少于三位'},
                                             {max:18,message:'密码不能超过18位'},
-                                            {pattern:/^[a-zA-Z0-9]{3,18}$/,message:'密码只能包含英文，数字，下划线'}
+                                            {pattern:/^[a-zA-Z0-9]{3,18}$/,message:'密码只能包含英文，数字，下划线'}*/
+
+                                            {validator:this.validator}
                                         ]
                                     }
                                     )(<Input perfix={<Icon type="lock"/>} placeholder="密码" type="password"/>)

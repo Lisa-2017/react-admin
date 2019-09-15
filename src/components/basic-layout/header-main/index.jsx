b0ba962243244e3d951cbe1fca82377541389f93
Leesa <18817396285@163.com>
2019-09-16 01:41:33 +0800

    screenFull点击全屏，全屏事件解绑避免内存泄漏

--- src/components/basic-layout/header-main/index.jsx
+++ src/components/basic-layout/header-main/index.jsx
@@ -1,13 +1,37 @@
 import React, {Component} from 'react'
 import {Button,Icon} from "antd";
 import './index.less'
+import  screenfull from 'screenfull'
 
 class HeaderMain extends Component {
+    state={
+        isScreenFull:false
+    }
+    screenfull=()=>{
+        if (screenfull.isEnabled) {
+            screenfull.toggle();
+        }
+    };
+    change =()=>{
+        this.setState({
+            isScreenFull:!this.props.isScreenFull
+        })
+    }
+
+    componentDidMount() {
+        // 绑定推出全屏事件
+        screenfull.on('change',this.change)
+    }
+    componentWillUnmount() {
+        screenfull.off('change',this.hange)
+    }
+
     render() {
+        const { isScreenFull } =this.state
         return (
             <div className="header-main">
                 <div  className="header-main-top">
-                    <Button size="small"><Icon type='fullscreen' /></Button>
+                    <Button size="small" onClick={this.screenfull}><Icon type={isScreenFull?'fullscreen-exit':'fullscreen'} /></Button>
                     <Button>English</Button>
                     <span> 欢迎，xxx</span>
                     <Button type="link">退出</Button>

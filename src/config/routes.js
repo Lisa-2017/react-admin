09f29cbd04ad84c6dae47220948531cb11be4645
Leesa <18817396285@163.com>
2019-09-16 13:54:56 +0800

    商品/分类管理静态页面

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,17 @@
 // 管理多个路由使用数组的形式，数组里面包的是一个个的对象
 import Home from "../components/home";
+import Category from "../components/basic-layout/category";
 
 const  routes = [
     {
         path:'/',
         exact:true,
         component:Home
+    },
+    {
+        path:'/category',
+        exact:true,
+        component:Category
     }
 ] ;
 
